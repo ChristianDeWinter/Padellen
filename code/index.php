a61dac297be5ab386c1ac2642e37165c7e43faf0
Christian_de_Winter <christian.bitacademy@gmail.com>
2024-09-12 12:28:10 +0200

    feat: Fixed next page location

--- code/index.php
+++ code/index.php
@@ -14,8 +14,7 @@
     <header class="header">
         <div class="header_container">
             <a class="header_brand" href="#">
-                <img class="brand__img" src="https://padelpoints.nl/wp-content/uploads/2021/07/logo.svg"
-                    alt="Padelpoints Logo">
+                <img class="brand__img" src="https://padelpoints.nl/wp-content/uploads/2021/07/logo.svg" alt="Padelpoints Logo">
             </a>
 
             <div class="header_feature">
@@ -51,14 +50,72 @@
         <div class="wp-block-group has-white-color has-text-color has-background centered-content" style="background-color:#292929">
             <div class="wp-block-group__inner-container is-layout-flow wp-block-group-is-layout-flow">
                 <div style="height:34px" aria-hidden="true" class="wp-block-spacer"></div>
-
                 <p class="has-text-align-center extra-small-align-left centered-text"><strong>Padel heeft het allemaal! Zin in een
                         potje padel? Boeken gaat simpel en snel via de website of Playtomic app. <br>Reserveer met 1
                         druk op de knop een padelbaan wanneer het jou uitkomt.</strong></p>
-
                 <div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
             </div>
         </div>
+
+        <section id="padel-banen-section-block_0f07311ffbc2ae383f1565932aed84df" class="padel-banen-section bg-figures-section" style="background-color: #1a1a1a">
+            <div class="on-bg">
+                <div class="container container--header">
+                    <h2 class="padel-banen-section__title">Onze padelbanen</h2>
+                </div>
+                <div class="container">
+                    <div class="row location__row">
+                        <div class="col-12 col-sm-8 col-lg-6">
+                            <article class="location">
+                                <div class="location__img">
+                                    <img decoding="async" src="https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-800x600.jpg" class="attachment-skywalker-950x600 size-skywalker-950x600 wp-post-image" alt="" srcset="https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-800x600.jpg 800w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-900x675.jpg 900w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-1200x900.jpg 1200w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-300x225.jpg 300w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-1024x768.jpg 1024w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-768x576.jpg 768w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-1536x1151.jpg 1536w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-2048x1535.jpg 2048w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-1534x1150.jpg 1534w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-500x375.jpg 500w, https://padelpoints.nl/wp-content/uploads/2021/07/PadelPoints-6-400x300.jpg 400w" sizes="(max-width: 800px) 100vw, 800px">
+                                </div>
+                                <hr>
+                                <div class="location__content">
+                                    <h3 class="location__title">
+                                        <span>Padelpoints </span>Badhoevedorp
+                                    </h3>
+                                    <p class="location__text">
+                                        Padelpoints Badhoevedorp is de grootste indoor padellocatie van Nederland: 14 padelbanen, een levendige horeca en gratis parkeren.
+                                    </p>
+                                    <div class="location__btns">
+                                        <a href="https://padelpoints.nl/padel_banen/badhoevedorp/" class="location__details-btn btn btn--transparent">
+                                            details
+                                        </a>
+                                        <a href="https://playtomic.io/padelpoints-badhoevedorp/0c65ed05-fcd1-44f9-baef-0eaa501ad071" class="location__external-btn btn" target="_blank" rel="nofollow">
+                                            reserveer
+                                        </a>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-12 col-sm-8 col-lg-6">
+                            <article class="location">
+                                <div class="location__img">
+                                    <img decoding="async" src="https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-801x600.jpeg" class="attachment-skywalker-950x600 size-skywalker-950x600 wp-post-image" alt="" srcset="https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-801x600.jpeg 801w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-300x225.jpeg 300w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-1024x768.jpeg 1024w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-768x576.jpeg 768w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-1536x1151.jpeg 1536w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-1534x1150.jpeg 1534w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-500x375.jpeg 500w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7-400x300.jpeg 400w, https://padelpoints.nl/wp-content/uploads/2022/07/E6EF17D6-97E2-42F9-8415-5657F8BE82B7.jpeg 2048w" sizes="(max-width: 801px) 100vw, 801px">
+                                </div>
+                                <hr>
+                                <div class="location__content">
+                                    <h3 class="location__title">
+                                        <span>Padelpoints </span>Hoofddorp
+                                    </h3>
+                                    <p class="location__text">
+                                        Padelpoints Hoofddorp is met 12 padelbanen een bruisende sportlocatie aan de rand van Amsterdam.
+                                    </p>
+                                    <div class="location__btns">
+                                        <a href="https://padelpoints.nl/padel_banen/hoofddorp/" class="location__details-btn btn btn--transparent">
+                                            details
+                                        </a>
+                                        <a href="https://playtomic.io/padelpoints-hoofddorp/8519afe9-dd55-4384-af68-9d6c4f8cfe03?q=PADEL~2022-09-26~~~" class="location__external-btn btn">
+                                            reserveer
+                                        </a>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 
     <script src="script.js"></script>
