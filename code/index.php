49e9572d86a3fbfe6297cb25dab251f96a1bc137
Christian_de_Winter <christian.bitacademy@gmail.com>
2024-10-01 13:57:51 +0200

    .\index.php

--- code/index.php
+++ code/index.php
@@ -247,22 +247,6 @@
                 speed: 500,
                 slidesToShow: 3,
                 slidesToScroll: 1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        }
-                    }
-                ]
             });
         });
     </script>
