b06695a0affd018d64432663d8891617a76d7c11
Christian_de_Winter <christian.bitacademy@gmail.com>
2024-10-03 14:13:24 +0200

    s

--- code/index.php
+++ code/index.php
@@ -68,7 +68,7 @@
                 </div>
                 <div class="container">
                     <div class="row location__row">
-                        <div class="col-12 col-sm-8 col-lg-6">
+                        <div class="col-12 col-sm-6">
                             <article class="location">
                                 <div class="location__img">
                                     <img decoding="async"
@@ -99,7 +99,7 @@
                                 </div>
                             </article>
                         </div>
-                        <div class="col-12 col-sm-8 col-lg-6">
+                        <div class="col-12 col-sm-6">
                             <article class="location">
                                 <div class="location__img">
                                     <img decoding="async"
@@ -134,161 +134,12 @@
                 </div>
             </div>
         </section>
-
-        <section id="trainers-section-block_f58c17f9da96f277f642df8574c38c11" class="trainers-section"
-            style="background-color: #005AA8">
-            <div class="container container--header">
-                <h2 class="trainers-section__title">Onze Trainers</h2>
-            </div>
-
-            <div class="trainers-section__row slick-initialized slick-slider slick-dotted" data-slick="trainers">
-                <div class="slick-list draggable" style="padding: 0px calc(-524px + 50vw);">
-                    <div class="slick-track" style="opacity: 1; width: 4124px; transform: translate3d(0px, 0px, 0px);">
-                        <div class="slick-slide slick-current slick-center" data-slick-index="0" aria-hidden="true"
-                            style="width: 1031px;" role="tabpanel" id="slick-slide00"
-                            aria-describedby="slick-slide-control00">
-                            <div>
-                                <article class="trainer" style="width: 100%; display: inline-block;">
-                                    <div class="trainer__wrap">
-                                        <div class="trainer__thumb">
-                                            <img decoding="async"
-                                                src="https://padelpoints.nl/wp-content/uploads/2023/08/C276C56D-FBD6-4178-AAD7-9D5D5589C84F.jpeg"
-                                                class="attachment-skywalker-500x500 size-skywalker-500x500 wp-post-image"
-                                                alt=""
-                                                srcset="https://padelpoints.nl/wp-content/uploads/2023/08/C276C56D-FBD6-4178-AAD7-9D5D5589C84F.jpeg 500w, https://padelpoints.nl/wp-content/uploads/2023/08/C276C56D-FBD6-4178-AAD7-9D5D5589C84F-300x225.jpeg 300w, https://padelpoints.nl/wp-content/uploads/2023/08/C276C56D-FBD6-4178-AAD7-9D5D5589C84F-400x300.jpeg 400w"
-                                                sizes="(max-width: 500px) 100vw, 500px">
-                                        </div>
-
-                                        <div class="trainer__content">
-                                            <div class="trainer__header">
-                                                <h3 class="trainer__name">Marcelino Dijkman</h3>
-
-                                                <p class="trainer__subs">‘Iedere les moet een belevenis zijn’</p>
-                                            </div>
-
-                                            <p class="trainer__text">
-                                                Marcelino Dijkman, dat is wie ik ben, en het is mijn passie om mijn
-                                                leerlingen de kunst van het padellen bij te brengen. Met essentiële en
-                                                waardevolle instructies zul je versteld staan van de snelheid waarmee je
-                                                vooruitgang boekt na slechts enkele lessen. Ik ben beschikbaar voor
-                                                zowel privélessen (1-2 personen) als groepslessen (4-8 personen). Leuk
-                                                en leerzaam!
-                                            </p>
-                                            <div class="trainer__btns">
-
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                </article>
-                                <article class="trainer" style="width: 100%; display: inline-block;">
-                                    <div class="trainer__wrap">
-                                        <div class="trainer__thumb">
-                                            <img decoding="async"
-                                                src="https://padelpoints.nl/wp-content/uploads/2023/08/5546C1A3-D906-4278-B3DB-FAE4DC842BCB_4_5005_c.jpeg"
-                                                class="attachment-skywalker-500x500 size-skywalker-500x500 wp-post-image"
-                                                alt="Sacha Scheepens" />
-                                        </div>
-                                        <div class="trainer__content">
-                                            <div class="trainer__header">
-                                                <h3 class="trainer__name">Sacha Scheepens-Schultz</h3>
-                                                <p class="trainer__subs">‘Plezier staat voorop’</p>
-                                            </div>
-                                            <p class="trainer__text">Mijn naam is Sacha Scheepens-Schultz. Met meer dan
-                                                20 jaar coachervaring en een achtergrond als tennisster, ben ik verheugd
-                                                om mijn passie voor padel met jullie te delen. Mijn doel is simpel: ik
-                                                wil dat je van padel geniet en je spel naar een hoger niveau tilt. Samen
-                                                gaan we werken aan je techniek, strategie en plezier op de baan. Of je
-                                                nu nieuw bent in de sport of je vaardigheden wilt verbeteren, ik sta
-                                                voor je klaar. Schrijf je in voor mijn lessen en laten we aan de slag
-                                                gaan!</p>
-                                            <div class="trainer__btns">
-                                                <a href="https://padelpoints.nl/padel-training/#trainers-section-block_60f1473650562"
-                                                    class="trainer__btn trainer__btn--archive btn btn--transparent">Alle
-                                                    trainers</a>
-                                                <a href="https://padelpoints.nl/padel-training/#lessons-block_02b212a2a358a7bddc0c53fe6c7b1e9f"
-                                                    class="trainer__btn btn btn--white">Boek een training</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article>
-
-                                <article class="trainer" style="width: 100%; display: inline-block;">
-                                    <div class="trainer__wrap">
-                                        <div class="trainer__thumb">
-                                            <img decoding="async"
-                                                src="https://padelpoints.nl/wp-content/uploads/2023/09/Borja-Camacho-1-500x500.jpg"
-                                                class="attachment-skywalker-500x500 size-skywalker-500x500 wp-post-image"
-                                                alt="Borja Camacho" />
-                                        </div>
-                                        <div class="trainer__content">
-                                            <div class="trainer__header">
-                                                <h3 class="trainer__name">Borja Camacho</h3>
-                                                <p class="trainer__subs">“Nieuwsgierigheid is één van mijn drijfveren”
-                                                </p>
-                                            </div>
-                                            <p class="trainer__text">Padel en tennis zijn altijd essentieel geweest in
-                                                mijn leven. Ik begon al op 10-jarige leeftijd en ben sindsdien niet meer
-                                                gestopt met het gepassioneerd spelen van deze balsporten. Als coach
-                                                geniet ik ervan om mijn leerlingen te zien groeien en plezier te hebben.
-                                                Mijn nieuwsgierigheid en drang naar kennis motiveren me om altijd nieuwe
-                                                dingen te proberen. Ik ben van Spaanse afkomst en spreek Nederlands,
-                                                Engels en uiteraard Spaans!</p>
-                                            <div class="trainer__btns">
-                                                <a href="https://padelpoints.nl/padel-training/#trainers-section-block_60f1473650562"
-                                                    class="trainer__btn trainer__btn--archive btn btn--transparent">Alle
-                                                    trainers</a>
-                                                <a href="https://padelpoints.nl/padel-training/#lessons-block_02b212a2a358a7bddc0c53fe6c7b1e9f"
-                                                    class="trainer__btn btn btn--white"><span>Boek een
-                                                        training</span></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article>
-
-                                <article class="trainer" style="width: 100%; display: inline-block;">
-                                    <div class="trainer__wrap">
-                                        <div class="trainer__thumb">
-                                            <img decoding="async"
-                                                src="https://padelpoints.nl/wp-content/uploads/2024/01/B412C506-74D7-454B-8057-2E82211D4762_1_201_a-463x500.jpeg"
-                                                class="attachment-skywalker-500x500 size-skywalker-500x500 wp-post-image"
-                                                alt="Rutger van Munster" />
-                                        </div>
-                                        <div class="trainer__content">
-                                            <div class="trainer__header">
-                                                <h3 class="trainer__name">Rutger van Munster</h3>
-                                                <p class="trainer__subs">“Laten we samen aan de slag gaan.”</p>
-                                            </div>
-                                            <p class="trainer__text">Ben je padel verslaafd of overweeg je het eens uit
-                                                te proberen met wat lessen? Ongeacht je motivatie, leeftijd of huidige
-                                                vaardigheidsniveau, laten we samen aan de slag gaan! Van jongs af aan
-                                                ben ik actief geweest in verschillende sporten, voornamelijk tennis en
-                                                voetbal. Mijn eerste kennismaking met padel vond plaats in 2005 in
-                                                Spanje, en ik was meteen verkocht! Sinds 2022 ben ik trainer en vind het
-                                                geweldig om mijn enthousiasme voor het spel over te brengen en anderen
-                                                te helpen hun spel te verbeteren. Ben jij de volgende? Abonneer je nu!
-                                                Vamos!</p>
-                                            <div class="trainer__btns">
-                                                <a href="https://padelpoints.nl/padel-training/#trainers-section-block_60f1473650562"
-                                                    class="trainer__btn trainer__btn--archive btn btn--transparent">Alle
-                                                    trainers</a>
-                                                <a href="https://padelpoints.nl/padel-training/#lessons-block_02b212a2a358a7bddc0c53fe6c7b1e9f"
-                                                    class="trainer__btn btn btn--white">Boek een training</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
     </main>
 
     <footer class="footer">
-        <p>&copy; 2023 Padel Points. Alle rechten voorbehouden.</p>
+        <div class="footer_container">
+            <p>&copy; 2024 Padel Points. All Rights Reserved.</p>
+        </div>
     </footer>
 </body>
 
